8800070fe33b83409fde74a77e539589ca154074
Gene Kogan <kogan.gene@gmail.com>
2019-04-08 17:09:35 -0400

    class 2

--- class.js
+++ class.js
@@ -1,2 +1,35 @@
+// var request = require('request');
+        
+// var url = process.argv[2];
+
+// request(url, function(error, response, html) {
+//     console.log(html);
+// });
+
 var fs = require('fs');
 
+var html = "<html><head></head><body><h1>" + Math.random() + "</h1></body></html>";
+
+fs.writeFile('part-2b.html', html, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+
+
+// ----------
+
+var fs = require('fs');
+
+var INPUT = 'input.txt';
+var OUTPUT = 'part-2c.html';
+
+var input = fs.readFileSync(INPUT, "utf-8");
+var lines = input.split("\n");
+
+fs.writeFileSync(OUTPUT, "<html><head></head><body>");
+
+for (var i=0; i<lines.length; i++) {
+    fs.appendFileSync(OUTPUT, "<h1>" + lines[i] + "</h1>");
+}
+fs.appendFileSync(OUTPUT, "</body></html>");
---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var mustacheExpress = require('mustache-express');
+
+var app = express()
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname);
+
+app.get('/', function(req, res) {
+        res.send('<html> <head></head> <body> <form method="get" action="."> Enter your name: <input type="text" name="name"> <input type="submit"> </form> </body> </html>');
+    }
+)
+
+app.get('/dwd', function(req, res) {
+        res.send("Our second route...");
+    }
+)
+
+app.listen(8000, function() {
+    console.log('Our first web server! Started on port 8000')
+})
